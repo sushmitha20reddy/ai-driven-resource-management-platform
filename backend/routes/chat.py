fc787fda249ac9e0791d52cb549f913822b334a1
kamalakarreddy <sowjanaya@Sushmithas-MacBook-Pro.local>
2026-06-11 15:16:49 +0530

    Added file upload support

--- backend/routes/chat.py
+++ backend/routes/chat.py
@@ -1,7 +1,4 @@
-# routes/chat.py
-
-from fastapi import APIRouter
-from pydantic import BaseModel
+from fastapi import APIRouter, UploadFile, File, Form
 from openai import OpenAI
 import os
 
@@ -11,23 +8,28 @@ client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
     api_key=os.getenv("OPENROUTER_API_KEY")
 )
-
-class ChatRequest(BaseModel):
-    message: str
-
 @router.post("/chat")
-async def chat(data: ChatRequest):
+async def chat(
+    message: str = Form(...),
+    file: UploadFile = File(None)
+):
+
+    print("Message:", message)
+
+    if file:
+        print("File Name:", file.filename)
 
     response = client.chat.completions.create(
         model="openai/gpt-3.5-turbo",
         messages=[
             {
                 "role": "user",
-                "content": data.message
+                "content": message
             }
         ]
     )
 
     return {
-        "response": response.choices[0].message.content
+        "response":
+        response.choices[0].message.content
     }
